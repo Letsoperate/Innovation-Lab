4589751adc833c7737b485a5cad276a59ff47dae
lintshiwe <ntoampilp@gmail.com>
2026-05-21 23:16:50 +0200

    fix: axios interceptor ensures token on every API request

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,9 +7,12 @@ const api = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-const token = localStorage.getItem("token");
-if (token) {
-  api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-}
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default api;
